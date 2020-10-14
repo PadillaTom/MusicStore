c64314fe2b9f299ce11b14eac0a33484eec09159
Tomas Padilla <padillatomasagustin@gmail.com>
2020-10-14 18:39:57 +0200

    comenzar sidebar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,31 +3,34 @@ import { Link } from 'react-router-dom';
 // Icons:
 import { HiMenuAlt2 } from 'react-icons/hi';
 import { BsBag } from 'react-icons/bs';
+// Components:
+import Sidebar from './Sidebar';
 
 const Navbar = () => {
   return (
-    <nav className='mobile-nav'>
-      <div className='mobile-nav-container'>
-        {/* Hamburg */}
-
-        <HiMenuAlt2 className='ham-icon'></HiMenuAlt2>
-
-        {/* End Hamburg */}
-        {/* Logo */}
-        <div className='logo-container'>
-          <Link to='/'>
-            <h2 className='main-logo'>Music Store</h2>
-          </Link>
+    <React.Fragment>
+      <nav className='mobile-nav'>
+        <div className='mobile-nav-container'>
+          {/* Hamburg */}
+          <HiMenuAlt2 className='ham-icon'></HiMenuAlt2>
+          {/* End Hamburg */}
+          {/* Logo */}
+          <div className='logo-container'>
+            <Link to='/'>
+              <h2 className='main-logo'>Music Store</h2>
+            </Link>
+          </div>
+          {/* End Logo */}
+          {/* Cart */}
+          <div className='cart-container'>
+            <BsBag className='cart-bag'></BsBag>
+            <p className='bag-amount'>1</p>
+          </div>
+          {/* End Cart */}
         </div>
-        {/* End Logo */}
-        {/* Cart */}
-        <div className='cart-container'>
-          <BsBag className='cart-bag'></BsBag>
-          <p className='bag-amount'>1</p>
-        </div>
-        {/* End Cart */}
-      </div>
-    </nav>
+      </nav>
+      <Sidebar></Sidebar>
+    </React.Fragment>
   );
 };
 
