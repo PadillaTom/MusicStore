ca63245c89fc08f9226f33b0d348e79064637f1d
Tomas Padilla <padillatomasagustin@gmail.com>
2020-10-15 11:35:35 +0200

    start isntruments, articles

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>Hello from About Page</h1>
-    </div>
+    <section className='section about-sect'>
+      <div className='section-title'>
+        <h1>About us</h1>
+      </div>
+      <div className='section-center'>
+        <img
+          src='https://firebasestorage.googleapis.com/v0/b/music-store-c8b9c.appspot.com/o/Others%2Fstore1.jpg?alt=media&token=89aa9262-9b60-4f84-ab25-9032b563d268'
+          alt='our-store'
+          className='about-img'
+        />
+        <div className='about-info'>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quod
+            fugiat id consequatur est commodi, perferendis explicabo maiores
+            aspernatur. Nihil quam, maiores adipisci libero dolores, id
+            repellendus autem est tempore odit molestias? Placeat, aperiam iure.
+          </p>
+        </div>
+        <Link to='/instruments'>
+          <button className='btn-about'>View Collection</button>
+        </Link>
+      </div>
+    </section>
   );
 };
 
--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -2,9 +2,69 @@ import React from 'react';
 
 const ContactPage = () => {
   return (
-    <div>
-      <h1>Hello from Contact Page</h1>
-    </div>
+    <section className='section contact-sect'>
+      <div className='section-title'>
+        <h2>Contact Us</h2>
+      </div>
+      <div className='section-center'>
+        <div className='contact-subtitle'>
+          <p>
+            Music Store also applies the same quality standards when it comes to
+            the customer service, please contact us!
+          </p>
+        </div>
+        <form className='contact-form'>
+          <div className='form-control'>
+            <div className='single-control'>
+              <label htmlFor='name' className='single-label'>
+                Name
+              </label>
+              <input
+                type='text'
+                placeholder='Your Name'
+                name='name'
+                className='single-input'
+              />
+            </div>
+            <div className='single-control'>
+              <label htmlFor='email' className='single-label'>
+                Email
+              </label>
+              <input
+                type='text'
+                placeholder='Your E-mail'
+                name='email'
+                className='single-input'
+              />
+            </div>
+            <div className='single-control'>
+              <label htmlFor='phone' className='single-label'>
+                Phone
+              </label>
+              <input
+                type='text'
+                placeholder='Phone Number'
+                name='phone'
+                className='single-input'
+              />
+            </div>
+            <div className='form-msg-control'>
+              <label htmlFor='message' className='single-label'>
+                Enter a Message
+              </label>
+              <textarea
+                name='message'
+                placeholder='Your Message...'
+                rows='10'
+                className='form-msg'
+              ></textarea>
+              <br />
+            </div>
+            <button className='btn-form'>Send</button>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 };
 
