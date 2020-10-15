c1a2233670dc732afb176d8c5a956dd9f7fb9054
Tomas Padilla <padillatomasagustin@gmail.com>
2020-10-15 12:48:02 +0200

    usar productscontext para filtarar bajos y guitarras

--- src/Pages/BassesPage.js
+++ src/Pages/BassesPage.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import db from '../firebase';
 
 const BassesPage = () => {
+  const [bassses, setBasses] = useState([]);
+
   return (
     <div>
       <h1>Hello from Basses Page</h1>
--- src/Pages/GuitarsPage.js
+++ src/Pages/GuitarsPage.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import db from '../firebase';
 
 const GuitarsPage = () => {
+  const [guitars, setGuitars] = useState([]);
+
   return (
     <div>
       <h1>Hello from Guitars Page</h1>
